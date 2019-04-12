8597ff710860da70cd1d76195164a2ddb3d31bd8
Eumenides997 <646224348@qq.com>
2019-04-12 18:08:04 +0800

    常用类练习 张祯强 3171104131 第九次提交

---
+++ src/常用类联系_4/text4.java
@@ -0,0 +1,14 @@
+package 常用类联系_4;
+
+public class text4 {
+	
+	public static void main(String[] args) {
+		
+		String s="(0000)0000-0000";
+		
+		System.out.println(s.matches("\\(\\d{4}\\)\\d{4}\\-\\d{4}"));
+		//第(1)题
+		
+	}
+
+}
