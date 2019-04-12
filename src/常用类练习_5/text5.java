77c7b1f9e7501c8bb0c7f3d79a7ba499dcc6fcc8
Eumenides997 <646224348@qq.com>
2019-04-12 23:13:40 +0800

    常用类练习 张祯强 3171104131 第十九次提交

--- src/常用类练习_5/text5.java
+++ src/常用类练习_5/text5.java
@@ -2,8 +2,12 @@ package 常用类练习_5;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.time.temporal.ChronoUnit;
 import java.util.Scanner;
+
+import javax.swing.text.DateFormatter;
 
 public class text5 {
 
@@ -14,11 +18,23 @@ public class text5 {
 		String s=sc.next();
 		LocalDate s1 = LocalDate.parse(s);
 		LocalDate s2 = s1.plus(7,ChronoUnit.DAYS);
-		System.out.println(s2);
-		//控制台输入一个日期加上七天后输出
+		System.out.println(s2.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.MEDIUM)));
+		//控制台输入一个日期加上七天后输出MEDIUM格式的日期
 		
 		LocalDateTime nowdate=LocalDateTime.now();
-		System.out.println(nowdate);
+		System.out.println(nowdate.format(DateTimeFormatter
+				.ofLocalizedDateTime(FormatStyle.SHORT,FormatStyle.SHORT)));
+		//短格式
+		System.out.println(nowdate.format(DateTimeFormatter
+				.ofLocalizedDateTime(FormatStyle.MEDIUM,FormatStyle.MEDIUM)));
+	    //中等格式
+		System.out.println(nowdate.format(DateTimeFormatter
+				.ofLocalizedDateTime(FormatStyle.FULL,FormatStyle.MEDIUM)));
+		/*System.out.println(nowdate.format(DateTimeFormatter
+				.ofLocalizedDateTime(FormatStyle.LONG,FormatStyle.LONG)));
+		*///无法从时间2019-04-12t23:02中提取区域ID:21.473540400，报错了
+		
+		
 
 	}
 
