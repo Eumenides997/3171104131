b077a9c0edb54cbf657fd935067a946b7cd6caa9
Eumenides997 <646224348@qq.com>
2019-04-12 18:37:24 +0800

    常用类练习 张祯强 3171104131 第十次提交

--- src/常用类联系_4/text4.java
+++ src/常用类联系_4/text4.java
@@ -1,5 +1,8 @@
 package 常用类联系_4;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class text4 {
 	
 	public static void main(String[] args) {
@@ -9,6 +12,14 @@ public class text4 {
 		System.out.println(s.matches("\\(\\d{4}\\)\\d{4}\\-\\d{4}"));
 		//第(1)题
 		
+		String ipadress="5465as4d200.33.1.85aw4d5200.33.1.8a4s65d4a";
+		Pattern p=Pattern.compile("(\\d{1,3}\\.){3}\\d{1,3}");
+		Matcher m=p.matcher(ipadress);
+
+		while(m.find()) {
+			System.out.println(m.group());
+		}
+		//第(2)题  PS:照书上的ip地址表达式写的，可能有几率出错
 	}
 
 }
